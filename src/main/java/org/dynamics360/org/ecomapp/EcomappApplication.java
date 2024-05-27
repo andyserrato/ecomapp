e99038419bfb1bcc8167b89effb7f51dd49de182
andyserrato <andyserrato@dynamics360.org>
2024-05-27 12:02:29 +0200

    Created base ecomm app project

---
+++ src/main/java/org/dynamics360/org/ecomapp/EcomappApplication.java
@@ -0,0 +1,13 @@
+package org.dynamics360.org.ecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomappApplication.class, args);
+	}
+
+}
