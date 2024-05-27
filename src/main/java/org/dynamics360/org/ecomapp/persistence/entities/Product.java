ee25e73fd9cd57a194cc372679f22282610dfd1e
andyserrato <andyserrato@dynamics360.org>
2024-05-27 12:18:11 +0200

    * Created the product entity.

---
+++ src/main/java/org/dynamics360/org/ecomapp/persistence/entities/Product.java
@@ -0,0 +1,51 @@
+package org.dynamics360.org.ecomapp.persistence.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private Double price;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
